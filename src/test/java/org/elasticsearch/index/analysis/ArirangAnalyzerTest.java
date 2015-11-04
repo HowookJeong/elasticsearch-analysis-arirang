264ca8f09d365ba16c8aa22a856018022153b38f
HowookJeong <sophistlv@gmail.com>
2015-11-04 13:49:01 +0900

    mapped REST API to TestCode.

--- src/test/java/org/elasticsearch/index/analysis/ArirangAnalyzerTest.java
+++ src/test/java/org/elasticsearch/index/analysis/ArirangAnalyzerTest.java
@@ -2,7 +2,6 @@ package org.elasticsearch.index.analysis;
 
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
-import org.apache.lucene.analysis.ko.KoreanAnalyzer;
 import org.apache.lucene.analysis.ko.KoreanFilter;
 import org.apache.lucene.analysis.ko.KoreanTokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
@@ -38,13 +37,11 @@ public class ArirangAnalyzerTest {
   public void testArirangAnalyzerNamedAnalyzer() throws Exception {
     System.out.println("####### testArirangAnalyzerNamedAnalyzer #######");
 
+    // http://localhost:9200/test/_analyze?analyzer=arirang&text=한국 엘라스틱서치 사용자 그룹의 HENRY 입니다.&pretty=1
     Index index = new Index("test");
     Settings settings = settingsBuilder()
         .put("path.home", "/tmp")
         .put("index.analysis.analyzer.arirang.type", "arirang_analyzer")
-        .put("index.analysis.analyzer.arirang.tokenizer", "arirang_tokenizer")
-        .putArray("index.analysis.analyzer.arirang.filter", "uppercase", "trim", "arirang_ft")
-        .put("index.analysis.filter.arirang_ft.type", "arirang_filter")
         .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
         .build();
 
@@ -64,20 +61,21 @@ public class ArirangAnalyzerTest {
     AnalysisService analysisService = injector.getInstance(AnalysisService.class);
     NamedAnalyzer namedAnalyzer = analysisService.analyzer("arirang");
 
-    TokenStream stream = namedAnalyzer.tokenStream(null, query);
+    TokenStream tokenStream = namedAnalyzer.tokenStream(null, query);
 
-    CharTermAttribute termAtt = stream.addAttribute(CharTermAttribute.class);
+    CharTermAttribute termAtt = tokenStream.addAttribute(CharTermAttribute.class);
+    TypeAttribute typeAttr = tokenStream.addAttribute(TypeAttribute.class);
 
     try {
-      stream.reset();
+      tokenStream.reset();
 
-      while (stream.incrementToken()) {
-        System.out.println(termAtt.toString());
+      while (tokenStream.incrementToken()) {
+        System.out.println(termAtt.toString() + " [" + typeAttr.type() + "]");
       }
 
-      stream.end();
+      tokenStream.end();
     } finally {
-      stream.close();
+      tokenStream.close();
     }
   }
 
@@ -85,13 +83,13 @@ public class ArirangAnalyzerTest {
   public void testArirangAnalyzerTokenFilter() throws Exception {
     System.out.println("####### testArirangAnalyzerTokenFilter #######");
 
+    // http://localhost:9200/test/_analyze?tokenizer=arirang_tokenizer&filters=arirang_filter&text=한국 엘라스틱서치 사용자 그룹의 HENRY 입니다.&pretty=1
     Index index = new Index("test");
     Settings settings = settingsBuilder()
         .put("path.home", "/tmp")
         .put("index.analysis.analyzer.arirang.type", "custom")
         .put("index.analysis.analyzer.arirang.tokenizer", "arirang_tokenizer")
-        .putArray("index.analysis.analyzer.arirang.filter", "lowercase", "arirang_ft")
-        .put("index.analysis.filter.arirang_ft.type", "arirang_filter")
+        .put("index.analysis.analyzer.arirang.filter", "arirang_filter")
         .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
         .build();
 
@@ -109,52 +107,61 @@ public class ArirangAnalyzerTest {
         .createChildInjector(parentInjector);
 
     AnalysisService analysisService = injector.getInstance(AnalysisService.class);
-    TokenFilterFactory tokenFilter = analysisService.tokenFilter("arirang_ft");
+    TokenFilterFactory tokenFilter = analysisService.tokenFilter("arirang_filter");
     Tokenizer tokenizer = (new ArirangTokenizerFactory(index,settings,null,settings)).create();
 
     tokenizer.setReader(new StringReader(query));
-    TokenStream filterStream = tokenFilter.create(tokenizer);
-
-    CharTermAttribute termAtt = filterStream.addAttribute(CharTermAttribute.class);
-
-    try {
-      filterStream.reset();
-
-      while (filterStream.incrementToken()) {
-        System.out.println(termAtt.toString());
-      }
-
-      filterStream.end();
-    } finally {
-      filterStream.close();
-    }
-  }
-
-  @Test
-  public void testArirangAnalyzerProvider() throws Exception {
-    System.out.println("####### testArirangAnalyzerProvider #######");
-
-    Index index = new Index("test");
-    Settings settings = settingsBuilder()
-        .put("path.home", "/tmp")
-        .put("index.analysis.analyzer.arirang.type", "custom")
-        .put("index.analysis.analyzer.arirang.tokenizer", "arirang_tokenizer")
-        .putArray("index.analysis.analyzer.arirang.filter", "arirang_ft")
-        .put("index.analysis.filter.arirang_ft.type", "arirang_filter")
-        .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
-        .build();
-
-    ArirangAnalyzerProvider analyzerProvider = new ArirangAnalyzerProvider(index, settings, null, null, settings);
-    KoreanAnalyzer koreanAnalyzer = analyzerProvider.get(); // analyzer 만 적용.
-    TokenStream tokenStream = koreanAnalyzer.tokenStream(null, query);
+    TokenStream tokenStream = tokenFilter.create(tokenizer);
 
     CharTermAttribute termAtt = tokenStream.addAttribute(CharTermAttribute.class);
+    TypeAttribute typeAttr = tokenStream.addAttribute(TypeAttribute.class);
 
     try {
       tokenStream.reset();
 
       while (tokenStream.incrementToken()) {
-        System.out.println(termAtt.toString());
+        System.out.println(termAtt.toString() + " [" + typeAttr.type() + "]");
+      }
+
+      tokenStream.end();
+    } finally {
+      tokenStream.close();
+    }
+  }
+
+  @Test
+  public void testArirangCustomAnalyzer() throws Exception {
+    System.out.println("####### testArirangCustomAnalyzer #######");
+
+    // http://localhost:9200/test/_analyze?tokenizer=arirang_tokenizer&filters=arirang_filter,lowercase&text=한국 엘라스틱서치 사용자 그룹의 HENRY 입니다.&pretty=1
+    Index index = new Index("test");
+    Settings settings = settingsBuilder()
+        .put("path.home", "/tmp")
+        .put("index.analysis.analyzer.arirang.type", "custom")
+        .put("index.analysis.analyzer.arirang.tokenizer", "arirang_tokenizer")
+        .putArray("index.analysis.analyzer.arirang.filter", "arirang_filter", "lowercase")
+        .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
+        .build();
+
+    ArirangTokenizerFactory arirangTokenizerFactory = new ArirangTokenizerFactory(index, settings, null, settings);
+    Tokenizer tokenizer = arirangTokenizerFactory.create();
+    ArirangTokenFilterFactory arirangTokenFilterFactory = new ArirangTokenFilterFactory(index, settings, null, settings);
+    LowerCaseTokenFilterFactory lowerCaseTokenFilterFactory = new LowerCaseTokenFilterFactory(index, settings, null, settings);
+    TokenStream tokenStream;
+
+    tokenizer.setReader(new StringReader(query));
+
+    tokenStream = arirangTokenFilterFactory.create(tokenizer);
+    tokenStream = lowerCaseTokenFilterFactory.create(tokenStream);
+
+    CharTermAttribute termAtt = tokenStream.addAttribute(CharTermAttribute.class);
+    TypeAttribute typeAttr = tokenStream.addAttribute(TypeAttribute.class);
+
+    try {
+      tokenStream.reset();
+
+      while (tokenStream.incrementToken()) {
+        System.out.println(termAtt.toString() + " [" + typeAttr.type() + "]");
       }
 
       tokenStream.end();
@@ -167,7 +174,7 @@ public class ArirangAnalyzerTest {
   public void testKoreanAnalyzer() throws Exception {
     System.out.println("####### testKoreanAnalyzer #######");
 
-    KoreanAnalyzer koreanAnalyzer = new KoreanAnalyzer(); // analyzer 적용.
+    // http://localhost:9200/test/_analyze?tokenizer=arirang_tokenizer&filters=arirang_filter&text=한국 엘라스틱서치 사용자 그룹의 HENRY 입니다.&pretty=1
     KoreanTokenizer koreanTokenizer = new KoreanTokenizer();  // tokenizer 적용.
     koreanTokenizer.setReader(new StringReader(query));
 
