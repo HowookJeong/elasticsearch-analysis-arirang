e2b7a216b61b420ab1a03f1128292cd2b0919cfa
henry <sophistlv@gmail.com>
2016-03-17 14:58:36 +0900

    added debug message for reload action.

--- src/main/java/org/elasticsearch/rest/action/analysis/ArirangAnalyzerRestAction.java
+++ src/main/java/org/elasticsearch/rest/action/analysis/ArirangAnalyzerRestAction.java
@@ -4,6 +4,8 @@ import org.apache.lucene.analysis.ko.morph.MorphException;
 import org.apache.lucene.analysis.ko.utils.DictionaryUtil;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.rest.*;
 
@@ -11,6 +13,8 @@ import org.elasticsearch.rest.*;
  * Created by jeonghoug on 2016. 3. 16..
  */
 public class ArirangAnalyzerRestAction extends BaseRestHandler {
+  ESLogger LOG = Loggers.getLogger(ArirangAnalyzerRestAction.class);
+
   @Inject
   public ArirangAnalyzerRestAction(Settings settings, RestController controller, Client client) {
     super(settings, controller, client);
@@ -21,6 +25,8 @@ public class ArirangAnalyzerRestAction extends BaseRestHandler {
   @Override
   public void handleRequest(RestRequest request, RestChannel channel, Client client) {
     try {
+      LOG.debug("Reloaded arirang analyzer dictionary!!");
+
       DictionaryUtil.loadDictionary();
     } catch (MorphException me) {
       channel.sendResponse(new BytesRestResponse(RestStatus.NOT_ACCEPTABLE, "Failed which reload arirang analyzer dictionary!!"));
