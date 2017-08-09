f39421d99f0c81f405c46bd946e4617aa805d060
henry_정호욱 <hw.jeong@memebox.com>
2017-08-09 11:35:39 +0900

    # elasticsearch-analysis-arirang/issues/4 RestAction 등록에 대한 5.5 소스 코드 변경에 따른 반영.

--- src/main/java/org/elasticsearch/plugin/analysis/arirang/AnalysisArirangPlugin.java
+++ src/main/java/org/elasticsearch/plugin/analysis/arirang/AnalysisArirangPlugin.java
@@ -1,19 +1,41 @@
 package org.elasticsearch.plugin.analysis.arirang;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.elasticsearch.index.analysis.*;
-import org.elasticsearch.index.analysis.ArirangTokenFilterFactory;
-import org.elasticsearch.indices.analysis.AnalysisModule.AnalysisProvider;
-import org.elasticsearch.plugins.AnalysisPlugin;
-import org.elasticsearch.plugins.Plugin;
-
-import java.util.HashMap;
-import java.util.Map;
-
+import static java.util.Collections.singletonList;
 import static java.util.Collections.singletonMap;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+import org.apache.lucene.analysis.Analyzer;
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.IndexScopedSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsFilter;
+import org.elasticsearch.index.analysis.AnalyzerProvider;
+import org.elasticsearch.index.analysis.ArirangAnalyzerProvider;
+import org.elasticsearch.index.analysis.ArirangTokenFilterFactory;
+import org.elasticsearch.index.analysis.ArirangTokenizerFactory;
+import org.elasticsearch.index.analysis.TokenFilterFactory;
+import org.elasticsearch.index.analysis.TokenizerFactory;
+import org.elasticsearch.indices.analysis.AnalysisModule.AnalysisProvider;
+import org.elasticsearch.plugins.ActionPlugin;
+import org.elasticsearch.plugins.AnalysisPlugin;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestHandler;
+import org.elasticsearch.rest.action.analisys.ArirangAnalyzerRestAction;
 
-public class AnalysisArirangPlugin extends Plugin implements AnalysisPlugin {
+
+public class AnalysisArirangPlugin extends Plugin implements AnalysisPlugin, ActionPlugin {
+    @Override
+    public List<RestHandler> getRestHandlers(Settings settings, RestController restController, ClusterSettings clusterSettings,
+      IndexScopedSettings indexScopedSettings, SettingsFilter settingsFilter, IndexNameExpressionResolver indexNameExpressionResolver,
+      Supplier<DiscoveryNodes> nodesInCluster) {
+        return singletonList(new ArirangAnalyzerRestAction(settings, restController));
+    }
 
     @Override
     public Map<String, AnalysisProvider<TokenFilterFactory>> getTokenFilters() {
